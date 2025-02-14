6a4ec3ff21afa13c2b38f6dc9bb1c8094361bfe2
yonghyeonpark <llyyoo93@gmail.com>
2025-02-14 21:50:48 +0900

    feat: UserRoom 관련 상수 관리 클래스 구현

---
+++ src/main/java/prography/assignment/domain/userroom/UserRoomConstants.java
@@ -0,0 +1,7 @@
+package prography.assignment.domain.userroom;
+
+public class UserRoomConstants {
+
+    public static final String TEAM_RED = "RED";
+    public static final String TEAM_BLUE = "BLUE";
+}
