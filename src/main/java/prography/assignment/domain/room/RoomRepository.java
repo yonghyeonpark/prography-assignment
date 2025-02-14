3d14217a74a5ba71b68306a04381ee79b2774411
yonghyeonpark <llyyoo93@gmail.com>
2025-02-14 23:30:57 +0900

    feat: Room 목록, 단건 조회에 페치 조인 적용

--- src/main/java/prography/assignment/domain/room/RoomRepository.java
+++ src/main/java/prography/assignment/domain/room/RoomRepository.java
@@ -5,17 +5,29 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.Optional;
+
 public interface RoomRepository extends JpaRepository<Room, Integer> {
 
     // countQuery를 별도로 지정하지 않으면 불필요한 조인 발생
     @Query(
             value = """
-                    SELECT r FROM Room r
+                    SELECT r
+                    FROM Room r
                     JOIN FETCH r.host
                     """,
             countQuery = """
-                    SELECT COUNT(r) FROM Room r
+                    SELECT COUNT(r)
+                    FROM Room r
                     """
     )
     Page<Room> findAllWithHost(Pageable pageable);
+
+    @Query("""
+            SELECT r
+            FROM Room r
+            JOIN FETCH r.host
+            WHERE r.id = :roomId
+            """)
+    Optional<Room> findByIdWithHost(Integer roomId);
 }
