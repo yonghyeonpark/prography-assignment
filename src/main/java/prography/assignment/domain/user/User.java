1ac1277ea7d0520a579c64dbf032d2277b1d2a34
yonghyeonpark <llyyoo93@gmail.com>
2025-02-14 17:04:22 +0900

    feat: User 엔티티 구현

---
+++ src/main/java/prography/assignment/domain/user/User.java
@@ -0,0 +1,23 @@
+package prography.assignment.domain.user;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import prography.assignment.domain.BaseTimeEntity;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class User extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private Integer fakerId;
+    private String name;
+    private String email;
+    private String status; // WAIT, ACTIVE, NON_ACTIVE
+}
