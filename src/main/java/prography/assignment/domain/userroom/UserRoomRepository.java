dabe4db8fd706904b62d63137124760d70ea3863
yonghyeonpark <llyyoo93@gmail.com>
2025-02-14 21:50:13 +0900

    feat: User Id 기준 존재 여부 확인 메서드 구현

--- src/main/java/prography/assignment/domain/userroom/UserRoomRepository.java
+++ src/main/java/prography/assignment/domain/userroom/UserRoomRepository.java
@@ -3,4 +3,6 @@ package prography.assignment.domain.userroom;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRoomRepository extends JpaRepository<UserRoom, Integer> {
+
+    boolean existsByRoomHostId(int hostId);
 }
