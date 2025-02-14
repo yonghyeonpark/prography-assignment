e8565d3d63ecc407e7a2a2ba6983201ffc921174
yonghyeonpark <llyyoo93@gmail.com>
2025-02-14 23:15:06 +0900

    feat: Room 조회 시, Host도 같이 가져오도록 페치 조인 적용 메서드 구현

--- src/main/java/prography/assignment/domain/room/RoomRepository.java
+++ src/main/java/prography/assignment/domain/room/RoomRepository.java
@@ -1,6 +1,21 @@
 package prography.assignment.domain.room;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface RoomRepository extends JpaRepository<Room, Integer> {
+
+    // countQuery를 별도로 지정하지 않으면 불필요한 조인 발생
+    @Query(
+            value = """
+                    SELECT r FROM Room r
+                    JOIN FETCH r.host
+                    """,
+            countQuery = """
+                    SELECT COUNT(r) FROM Room r
+                    """
+    )
+    Page<Room> findAllWithHost(Pageable pageable);
 }
