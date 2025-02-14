af92880127d2b9960b6ee754274bb0ea643ad838
yonghyeonpark <llyyoo93@gmail.com>
2025-02-15 05:33:02 +0900

    feat: 조건에 따른 UserRoom 삭제 메서드 구현

--- src/main/java/prography/assignment/domain/userroom/UserRoomRepository.java
+++ src/main/java/prography/assignment/domain/userroom/UserRoomRepository.java
@@ -4,5 +4,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRoomRepository extends JpaRepository<UserRoom, Integer> {
 
-    boolean existsByRoomHostId(int hostId);
+    boolean existsByRoomHostId(Integer hostId);
+
+    int countByRoomIdAndRoomRoomType(Integer roomId, String roomType);
+
+    boolean existsByRoomIdAndUserId(Integer roomId, Integer userId);
+
+    void deleteByRoomId(Integer roomId);
+
+    void deleteByRoomIdAndUserId(Integer roomId, Integer userId);
 }
