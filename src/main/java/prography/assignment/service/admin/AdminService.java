cd74642fcb11185815be2b472a2f613f5ac78c61
yonghyeonpark <llyyoo93@gmail.com>
2025-02-15 22:05:19 +0900

    feat: 초기화 API 구현

--- src/main/java/prography/assignment/service/admin/AdminService.java
+++ src/main/java/prography/assignment/service/admin/AdminService.java
@@ -4,11 +4,18 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import prography.assignment.client.FakerClient;
+import prography.assignment.client.dto.response.FakerUser;
 import prography.assignment.domain.room.RoomRepository;
+import prography.assignment.domain.user.User;
 import prography.assignment.domain.user.UserRepository;
 import prography.assignment.domain.userroom.UserRoomRepository;
+import prography.assignment.web.admin.dto.request.InitRequest;
 import prography.assignment.web.admin.dto.response.UserResponse;
 import prography.assignment.web.admin.dto.response.UsersResponse;
+
+import java.util.Comparator;
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
@@ -17,12 +24,21 @@ public class AdminService {
     private final UserRepository userRepository;
     private final UserRoomRepository userRoomRepository;
     private final RoomRepository roomRepository;
+    private final FakerClient fakerClient;
 
-    public void init() {
+    public void init(InitRequest initRequest) {
         userRoomRepository.deleteAllInBatch();
         roomRepository.deleteAllInBatch();
         userRepository.deleteAllInBatch();
 
+        List<User> users = fakerClient.getData(initRequest.seed(), initRequest.quantity())
+                .data()
+                .stream()
+                .sorted(Comparator.comparing(FakerUser::id))
+                .map(FakerUser::toEntity)
+                .toList();
+
+        userRepository.saveAll(users);
     }
 
     public UsersResponse getUsers(Pageable pageable) {
--- src/main/java/prography/assignment/web/admin/AdminController.java
+++ src/main/java/prography/assignment/web/admin/AdminController.java
@@ -5,9 +5,12 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import prography.assignment.service.admin.AdminService;
 import prography.assignment.web.ApiResponse;
+import prography.assignment.web.admin.dto.request.InitRequest;
 import prography.assignment.web.admin.dto.response.UsersResponse;
 
 @RequiredArgsConstructor
@@ -21,11 +24,11 @@ public class AdminController {
         return ApiResponse.responseSuccess();
     }
 
-    /*@PostMapping("/init")
-    public ApiResponse<Void> init() {
-        adminService.init();
+    @PostMapping("/init")
+    public ApiResponse<Void> init(@RequestBody InitRequest initRequest) {
+        adminService.init(initRequest);
         return ApiResponse.responseSuccess();
-    }*/
+    }
 
     @GetMapping("/user")
     public ApiResponse<UsersResponse> getUsers(@PageableDefault(sort = "id", direction = Sort.Direction.ASC) Pageable pageable) {
