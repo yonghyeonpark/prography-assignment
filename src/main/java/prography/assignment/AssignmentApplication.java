c238e58898ef4330db9f0b8d40d9046f2366af72
yonghyeonpark <llyyoo93@gmail.com>
2025-02-15 22:04:40 +0900

    chore: openfeign 사용을 위한 어노테이션 추가

--- src/main/java/prography/assignment/AssignmentApplication.java
+++ src/main/java/prography/assignment/AssignmentApplication.java
@@ -2,12 +2,14 @@ package prography.assignment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class AssignmentApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AssignmentApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AssignmentApplication.class, args);
+    }
 
 }
