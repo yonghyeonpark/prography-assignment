3922ef255687a4f7f13838095b380164b84292b8
yonghyeonpark <llyyoo93@gmail.com>
2025-02-15 20:04:21 +0900

    refactor: 불필요한 메서드 삭제 및 엔티티 조회 메서드 추가

--- src/main/java/prography/assignment/domain/userroom/UserRoomRepository.java
+++ src/main/java/prography/assignment/domain/userroom/UserRoomRepository.java
@@ -2,15 +2,15 @@ package prography.assignment.domain.userroom;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRoomRepository extends JpaRepository<UserRoom, Integer> {
 
     boolean existsByRoomHostId(Integer hostId);
 
     int countByRoomIdAndRoomRoomType(Integer roomId, String roomType);
 
-    boolean existsByRoomIdAndUserId(Integer roomId, Integer userId);
+    Optional<UserRoom> findByRoomIdAndUserId(Integer roomId, Integer userId);
 
     void deleteByRoomId(Integer roomId);
-
-    void deleteByRoomIdAndUserId(Integer roomId, Integer userId);
 }
