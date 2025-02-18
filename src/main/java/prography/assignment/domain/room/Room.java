59587357e62dccfc85d79af362ad9a9c0db40d6f
yonghyeonpark <llyyoo93@gmail.com>
2025-02-18 18:19:02 +0900

    feat: Room 엔티티에 테스트용 생성자 추가

--- src/main/java/prography/assignment/domain/room/Room.java
+++ src/main/java/prography/assignment/domain/room/Room.java
@@ -8,11 +8,12 @@ import prography.assignment.domain.BaseTimeEntity;
 import prography.assignment.domain.user.User;
 import prography.assignment.exception.CommonException;
 
+import java.time.LocalDateTime;
+
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 public class Room extends BaseTimeEntity {
-
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -31,6 +32,23 @@ public class Room extends BaseTimeEntity {
             String title,
             String roomType
     ) {
+        this.host = host;
+        this.title = title;
+        this.roomType = roomType;
+        this.status = RoomConstants.WAIT;
+    }
+
+    // 테스트용 생성자
+    public Room(
+            Integer id,
+            User host,
+            String title,
+            String roomType,
+            LocalDateTime createdAt,
+            LocalDateTime updatedAt
+    ) {
+        super(createdAt, updatedAt);
+        this.id = id;
         this.host = host;
         this.title = title;
         this.roomType = roomType;
