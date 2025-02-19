892daf96d22496d4290e61162146ef42eebb59c1
yonghyeonpark <llyyoo93@gmail.com>
2025-02-19 17:22:37 +0900

    feat: 방 생성 시, roomType 검증 로직 추가

--- src/main/java/prography/assignment/web/room/dto/request/CreateRoomRequest.java
+++ src/main/java/prography/assignment/web/room/dto/request/CreateRoomRequest.java
@@ -3,11 +3,19 @@ package prography.assignment.web.room.dto.request;
 import prography.assignment.domain.room.Room;
 import prography.assignment.domain.user.User;
 
+import static prography.assignment.domain.room.RoomConstants.DOUBLE;
+import static prography.assignment.domain.room.RoomConstants.SINGLE;
+
 public record CreateRoomRequest(
         Integer userId,
         String title,
         String roomType
 ) {
+    public CreateRoomRequest {
+        if (!roomType.equals(SINGLE) && !roomType.equals(DOUBLE)) {
+            throw new RuntimeException();
+        }
+    }
 
     public Room toEntity(User host) {
         return new Room(
