591ec13f0b0e98770ce1818b98bdad639cb3c8ff
yonghyeonpark <llyyoo93@gmail.com>
2025-02-21 05:29:10 +0900

    refactor: 500 code에 대한 HttpStatus 지정

--- src/main/java/prography/assignment/exception/GlobalExceptionHandler.java
+++ src/main/java/prography/assignment/exception/GlobalExceptionHandler.java
@@ -1,8 +1,11 @@
 package prography.assignment.exception;
 
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import prography.assignment.web.common.ApiResponse;
+
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -12,6 +15,7 @@ public class GlobalExceptionHandler {
         return ApiResponse.responseImpossible();
     }
 
+    @ResponseStatus(INTERNAL_SERVER_ERROR)
     @ExceptionHandler(Exception.class)
     public ApiResponse<Void> handleRuntimeException() {
         return ApiResponse.responseServerError();
